2074f674b39d8e7bb7d0f0da43a2c67eb4ed0a22
Paul Bowsher <pbowsher@globalpersonals.co.uk>
2012-09-20 17:10:48 +0100

    Pass through options to Rails.cache.write

--- lib/memcaches_page.rb
+++ lib/memcaches_page.rb
@@ -1,15 +1,15 @@
 module MemcachesPage
   extend ActiveSupport::Concern
   module ClassMethods
-    def cache_page(content, path)
+    def cache_page(content, path, options={})
       return unless perform_caching
-      
-      Rails.cache.write path.gsub('%', '%25'), content, raw: true
+
+      Rails.cache.write path.gsub('%', '%25'), content, options.merge(raw: true)
     end
   end
-  
-  def cache_page(content = nil, options = nil)
-    return unless self.class.perform_caching && caching_allowed?
-    self.class.cache_page(content || response.body, request.fullpath)
+
+  def cache_page(content = nil, options = {})
+    return unless self.class.perform_caching && caching_allowed? && !request.params.key?('no-cache')
+    self.class.cache_page(content || response.body, request.fullpath, options)
   end
 end
--- memcaches_page.gemspec
+++ memcaches_page.gemspec
@@ -5,7 +5,7 @@ Gem::Specification.new do |gem|
   gem.description   = %q{Overrides caches_page and stores in memcached instead.}
   gem.summary       = %q{Uses configuration from Rails cache store.}
   gem.homepage      = ""
-  
+
   gem.add_dependency("rails", "~> 3.1")
 
   gem.files         = `git ls-files`.split($\)
@@ -13,5 +13,5 @@ Gem::Specification.new do |gem|
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = "memcaches_page"
   gem.require_paths = ["lib"]
-  gem.version       = '0.1.2'
+  gem.version       = '0.1.3'
 end
